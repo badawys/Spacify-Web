7b9cf1a658af111e9613a3e908389022161c6503
Ahmad Elbadawy <a7mad.elbadawy@outlook.com>
2016-11-30 06:24:00 +0200

    Space repository added

---
+++ app/Repositories/Api/Space/EloquentSpaceRepository.php
@@ -0,0 +1,69 @@
+<?php
+namespace app\Repositories\Api\Space;
+
+class EloquentSpaceRepository implements SpaceRepositoryContract {
+
+    /**
+     * @inheritDoc
+     */
+    public function create(array $data)
+    {
+        // TODO: Implement create() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function findById($id)
+    {
+        // TODO: Implement findById() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function findByName($name)
+    {
+        // TODO: Implement findByName() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function nearby($long, $lat)
+    {
+        // TODO: Implement nearby() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function update($id, array $data)
+    {
+        // TODO: Implement update() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function deactivate($id)
+    {
+        // TODO: Implement deactivate() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function reactivate($id)
+    {
+        // TODO: Implement reactivate() method.
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+}
