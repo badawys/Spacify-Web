35d11e7cd74328a2609c11e7f19533e2ff800afc
Samar Sherif <Samar Sherif>
2016-11-30 21:08:32 +0200

    Create new space

--- app/Http/Controllers/Api/v1/Space/SpaceController.php
+++ app/Http/Controllers/Api/v1/Space/SpaceController.php
@@ -6,11 +6,27 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use app\Http\Requests\Api\Space\CreateSpaceRequest;
+use app\Repositories\Api\Space\SpaceRepositoryContract;
+
+
 
 class SpaceController extends Controller
 {
-    public function createSpace()
-    {
-       return 'Samar';
-    }
+   protected $spaces;
+
+       public function __construct(SpaceRepositoryContract $spaces)
+       {
+           $this -> spaces = $spaces;
+       }
+
+       /**
+        * @return SpaceRepositoryContract
+        */
+       public function create(CreateSpaceRequest $request)
+       {
+           $newSpace = $this -> space ->create($request->all());
+
+           return $newSpace;
+       }
 }
